0f1427f5837a3ae3586bfdb02556bb39c3a20a50
Kim <kim.testa@programmer.net>
2018-09-20 11:43:07 +0800

    Correction

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,14 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends MY_Controller {
-	
-	public function __construct()
-	{
+	public function __construct() {
 		parent::__construct();
 	}
 	
-	public function index() //function name should be same on target view file e.g.(application/views/pages/index.php)
-	{
+	public function index() {//function name should be same on target view file e.g.(application/views/pages/index.php)
 		$this->view_data = [
 			"title" => "Success!", //page title
 			"styles" => [
@@ -21,7 +18,7 @@ class Main extends MY_Controller {
 		];  
 	}
 	
-	public function function_name(){
+	public function function_name() {
 
 	}
 }
